3bdb6515a33fd1b5a3883fc2da40ab306656e027
tofmat <tofmatt@gmail.com>
2020-04-13 17:48:46 +0100

    added cards

---
+++ src/components/JobCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-col md="3">
+          <b-card
+          title="This is a card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job"
+          img-top
+          tag='article'
+          style="max-width:20rem"
+          class="mb-2"
+          footer="This is a footer"
+          >
+            <b-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo cupiditate eius soluta placeat, assumenda consequuntur nisi perferendis beatae, doloremque debitis nulla libero iste sit officiis. Nostrum voluptas deserunt at sed.
+            </b-card-text>
+            <b-button variant="primary">Apply</b-button>
+           </b-card>
+        </b-col>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,28 @@
 <template>
     <b-container class="bv-example-row">
       <b-row align-v="center">
-        <b-col md="3">
-          <b-card
-          title="This is a card"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Job"
-          img-top
-          tag='article'
-          style="max-width:20rem"
-          class="mb-2"
-          >
-            <b-card-text>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo cupiditate eius soluta placeat, assumenda consequuntur nisi perferendis beatae, doloremque debitis nulla libero iste sit officiis. Nostrum voluptas deserunt at sed.
-            </b-card-text>
-            <b-button variant="primary">Apply</b-button>
-           </b-card>
-        </b-col>
+        <JobCard />
+        <JobCard />
+        <JobCard />
       </b-row>
     </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import JobCard from '../components/JobCard'
 export default {
   name: "Home",
   components: {
+    JobCard
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods: {
+    async fetchData(){
+      const res = await fetch 
+    }
   }
 };
 </script>
